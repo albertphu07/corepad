1d8a40e43309bff7127f7141321107f5582c1c83
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-07-29 13:21:58 -0500

    Create config.h

---
+++ qmk_firmware/corepad_firmware/config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 3
+
+//Switch Pins
+#define DIRECT_PINS { \
+{ GP26, GP27, GP28 } \
+}
+
+
+// Direct Connect to GND
+#define DIRECT_PINS_PULLUP
+
+#define DEBOUNCE 5
+
+// OLED I2C
+#define I2C_DRIVER I2CD1
+
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
+
+
+// Vial security ID
+#define VIAL_KEYBOARD_UID {0x12,0x34,0x56,0x78,0x9A,0xBC,0xDE,0xF0}
+
+
+#define MANUFACTURER "CorePad Technologies INC"
+#define PRODUCT "CorePad Lite"
